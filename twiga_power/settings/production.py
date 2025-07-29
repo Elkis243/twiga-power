f6eef2c9ea5050a9214f76292270a043f18c2f7f
Elkis243 <elielubama@gmail.com>
2025-07-29 11:53:54 +0100

    Ajout de la structure de base du projet Django avec les fichiers de configuration, les modèles, les vues, les tests et les fichiers statiques.

---
+++ twiga_power/settings/production.py
@@ -0,0 +1,19 @@
+from .base import *
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALLOWED_HOSTS = ['twigapower.com']
+
+SECURE_HSTS_SECONDS = int(os.getenv('SECURE_HSTS_SECONDS'))
+SECURE_HSTS_INCLUDE_SUBDOMAINS = os.getenv('SECURE_HSTS_INCLUDE_SUBDOMAINS') == 'True'
+SECURE_HSTS_PRELOAD = os.getenv('SECURE_HSTS_PRELOAD') == 'True'
+SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT') == 'True'
+
+SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE') == 'True'
+CSRF_COOKIE_SECURE = os.getenv('CSRF_COOKIE_SECURE') == 'True'
+CSRF_TRUSTED_ORIGINS = ['https://twigapower.com']
+
+X_FRAME_OPTIONS = os.getenv('X_FRAME_OPTIONS')
+SECURE_BROWSER_XSS_FILTER = os.getenv('SECURE_BROWSER_XSS_FILTER') == 'True'
+SECURE_CONTENT_TYPE_NOSNIFF = os.getenv('SECURE_CONTENT_TYPE_NOSNIFF') == 'True'
