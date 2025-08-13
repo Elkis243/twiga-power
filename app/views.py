a037bf9cd35c95561a74d8ae20ef3a15b7540e36
Elkis243 <elielubama@gmail.com>
2025-08-13 10:40:57 +0100

    Add contact and employment pages, update footer and navbar components
    - Created a new contact page with a form for user inquiries.
    - Added an employment page indicating the site is under construction.
    - Implemented a footer component with company information and social media links.
    - Developed a navbar component for site navigation, including links to home, services, employment, and contact pages.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    page = "Home"
+    context = {
+        'page': page
+    }
+    return render(request, 'app/home.html', context)
+
+def contact(request):
+    page = "Contact"
+    context = {
+        'page': page
+    }
+    return render(request, 'app/contact.html', context)
+
+
+def employment(request):
+    page = "Emploi"
+    context = {
+        'page': page
+    }
+    return render(request, 'app/employment.html', context)
